fdc9eb789c950ff6d39cf8be98d760656d11dbef
lizion2003 <lizion2003@yahoo.com.hk>
2024-02-20 10:39:04 -0800

    Add index page, login page, add nav bar and footer

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -1,14 +1,14 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
-};
+const firebaseConfig = {
+    apiKey: "AIzaSyAscF4zGUKKqFFUTNqUpFqVbomAS2tDG1U",
+    authDomain: "comp1800-demo-4f8b2.firebaseapp.com",
+    projectId: "comp1800-demo-4f8b2",
+    storageBucket: "comp1800-demo-4f8b2.appspot.com",
+    messagingSenderId: "685566530343",
+    appId: "1:685566530343:web:273f5207346786ea48cf02"
+  };
 
 //--------------------------------------------
 // initialize the Firebase app
